89b29d2f4d8957c2da02172f2cda0d94bf51be85
whywaita <whywaita@whywrite.it>
2021-08-05 11:26:56 +0900

    Add scope input

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,42 @@
-import {createAppAuth} from '@octokit/auth-app';
-import {Octokit} from '@octokit/rest';
-import * as core from '@actions/core';
+import { createAppAuth } from "@octokit/auth-app";
+import { Octokit } from "@octokit/rest";
+import { Endpoints } from "@octokit/types";
+import * as core from "@actions/core";
 
 async function run(): Promise<void> {
   try {
-    const privateKey: string = core.getInput('private_key');
-    const id: string = core.getInput('app_id');
+    const privateKey: string = core.getInput("private_key");
+    const id: string = core.getInput("app_id");
+    const scope: string = core.getInput("scope");
     const appOctokit = new Octokit({
       authStrategy: createAppAuth,
       auth: {
         id,
         privateKey,
       },
-      baseUrl: process.env['GITHUB_API_URL'] || 'https://api.github.com',
+      baseUrl: process.env.GITHUB_API_URL || "https://api.github.com",
     });
 
-    const {data} = await appOctokit.apps.listInstallations();
+    type listInstallationsResponse = Endpoints["GET /app/installations"]["response"];
+
+    const installations: listInstallationsResponse = await appOctokit.apps.listInstallations();
+    let installationId = installations.data[0].id;
+    if (scope !== "") {
+      const scopedData = installations.data.find(
+        (item) => item.account.login === scope
+      );
+      if (scopedData === undefined) {
+        throw new Error(`set scope is ${scope}, but installation is not found`);
+      }
+      installationId = scopedData.id;
+    }
 
     const resp = await appOctokit.auth({
-      type: 'installation',
-      installationId: data[0].id,
+      type: "installation",
+      installationId,
     });
     // @ts-ignore
-    core.setOutput('token', resp.token);
+    core.setOutput("token", resp.token);
   } catch (error) {
     core.setFailed(error.message);
   }
