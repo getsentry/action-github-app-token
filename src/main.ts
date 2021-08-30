38a3ce582e170ddfe8789f509597c6944f2292a9
Billy Vong <billyvg@users.noreply.github.com>
2021-08-30 12:48:42 -0700

    feat: Upgrade `actions/core` and `octokit` deps (#16)
    Also updates dev deps to latest and deduplicates yarn lockfile.

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,25 @@ import {Octokit} from '@octokit/rest';
 import {Endpoints} from '@octokit/types';
 import * as core from '@actions/core';
 
-type listInstallationsResponse = Endpoints['GET /app/installations']['response'];
+type listInstallationsResponse =
+  Endpoints['GET /app/installations']['response'];
 
 async function run(): Promise<void> {
   try {
     const privateKey: string = core.getInput('private_key');
-    const id: string = core.getInput('app_id');
+    const appId: string = core.getInput('app_id');
     const scope: string = core.getInput('scope');
     const appOctokit = new Octokit({
       authStrategy: createAppAuth,
       auth: {
-        id,
+        appId,
         privateKey,
       },
       baseUrl: process.env.GITHUB_API_URL || 'https://api.github.com',
     });
 
-    const installations: listInstallationsResponse = await appOctokit.apps.listInstallations();
+    const installations: listInstallationsResponse =
+      await appOctokit.apps.listInstallations();
     let installationId = installations.data[0].id;
     if (scope !== '') {
       const scopedData = installations.data.find(
@@ -47,7 +49,9 @@ async function run(): Promise<void> {
     // @ts-expect-error
     core.setOutput('token', resp.token);
   } catch (error) {
-    core.setFailed(error.message);
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
   }
 }
 
