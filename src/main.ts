bf85f1ce2e026693034f6b14d00a7364c2e04b94
Billy Vong <billyvg@users.noreply.github.com>
2021-08-30 12:16:51 -0700

    feat: Add CI, fix TS errors, apply prettier (#15)
    Add CI tests and fix an existing TS error. Fix eslint and apply prettier formatting.

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
-import { createAppAuth } from "@octokit/auth-app";
-import { Octokit } from "@octokit/rest";
-import { Endpoints } from "@octokit/types";
-import * as core from "@actions/core";
+import {createAppAuth} from '@octokit/auth-app';
+import {Octokit} from '@octokit/rest';
+import {Endpoints} from '@octokit/types';
+import * as core from '@actions/core';
 
-type listInstallationsResponse = Endpoints["GET /app/installations"]["response"];
+type listInstallationsResponse = Endpoints['GET /app/installations']['response'];
 
 async function run(): Promise<void> {
   try {
-    const privateKey: string = core.getInput("private_key");
-    const id: string = core.getInput("app_id");
-    const scope: string = core.getInput("scope");
+    const privateKey: string = core.getInput('private_key');
+    const id: string = core.getInput('app_id');
+    const scope: string = core.getInput('scope');
     const appOctokit = new Octokit({
       authStrategy: createAppAuth,
       auth: {
         id,
         privateKey,
       },
-      baseUrl: process.env.GITHUB_API_URL || "https://api.github.com",
+      baseUrl: process.env.GITHUB_API_URL || 'https://api.github.com',
     });
 
     const installations: listInstallationsResponse = await appOctokit.apps.listInstallations();
     let installationId = installations.data[0].id;
-    if (scope !== "") {
+    if (scope !== '') {
       const scopedData = installations.data.find(
-        (item) => item.account.login === scope
+        (item) => item.account?.login === scope
       );
       if (scopedData === undefined) {
         throw new Error(`set scope is ${scope}, but installation is not found`);
@@ -31,13 +31,21 @@ async function run(): Promise<void> {
       installationId = scopedData.id;
     }
 
+    // This is untyped
+    // See: https://github.com/octokit/core.js/blob/master/src/index.ts#L182-L183
     const resp = await appOctokit.auth({
-      type: "installation",
+      type: 'installation',
       installationId,
     });
+
+    if (!resp) {
+      throw new Error('Unable to authenticate');
+    }
+
+    // @ts-expect-error
     core.setSecret(resp.token);
-    // @ts-ignore
-    core.setOutput("token", resp.token);
+    // @ts-expect-error
+    core.setOutput('token', resp.token);
   } catch (error) {
     core.setFailed(error.message);
   }
